a37431c86aeb1ac857b3a1ec6cf94a0975e44a2b
ItsClaudeee4 <164259725+ItsClaudeee4@users.noreply.github.com>
2025-07-25 04:17:37 +0100

    seond

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const allRoutes = require("./routes/allRoutes");
 const mongoose = require("mongoose");
 app.set("view engine", "ejs");
